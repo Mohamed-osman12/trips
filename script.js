6d6d7295429075d8cd0627d97c860f4c48872106
Mohamed-osman12 <139075713+Mohamed-osman12@users.noreply.github.com>
2024-06-28 04:14:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Firebase configuration
     const firebaseConfig = {
-        apiKey: "AIzaSyBiNZmf6rBGK0u61jdJRjPYqSTTV9LHVHs",
-        authDomain: "trips-b722c.firebaseapp.com",
-        projectId: "trips-b722c",
-        storageBucket: "trips-b722c.appspot.com",
-        messagingSenderId: "73446183139",
-        appId: "1:73446183139:web:81eea9689917ed15ca75d6",
-        measurementId: "G-VWMC813WK0"
+      apiKey: "AIzaSyBiNZmf6rBGK0u61jdJRjPYqSTTV9LHVHs",
+      authDomain: "trips-b722c.firebaseapp.com",
+      projectId: "trips-b722c",
+      storageBucket: "trips-b722c.appspot.com",
+      messagingSenderId: "73446183139",
+      appId: "1:73446183139:web:81eea9689917ed15ca75d6",
+      measurementId: "G-VWMC813WK0"
     };
 
     // Initialize Firebase
